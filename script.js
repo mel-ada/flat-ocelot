56c1dca84ca5db9fd99f1be9cf1b3aa4e5e6544f
Melissa Morel <melmorel06@gmail.com>
2016-11-30 17:13:08 -0800

    tutorial 33

--- script.js
+++
@@ -1,9 +0,0 @@
-//Each item should look like this:
-//<li><input type="checkbox" /><span>Write this tutorial</span></li>
-
-function addItem(){
-    var node = document.createElement("LI");
-    var textnode = document.createTextNode(document.forms["checkListForm"]["checkListItem"].value);
-    node.appendChild(textnode);
-    document.getElementById("todoList").appendChild(node);
-}
